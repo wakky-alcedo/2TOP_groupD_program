97db4b3eea73d3353a7dcc1a1c07f25b7bb78734
wakky-alcedo <wakky.alcedo@gmail.com>
2024-08-22 15:56:30 +0800

    add: led blink

--- single_test/single_test.ino
+++ single_test/single_test.ino
@@ -3,6 +3,8 @@ int IN2 = 8;
 int ENA = 9;
 int relayPin = 10;
 
+const int ledPin = LED_BUILTIN;  // the number of the LED pin
+
 bool Run = 0;  // Status set up
 
 void setup() {
@@ -11,11 +13,17 @@ void setup() {
   pinMode(IN1, OUTPUT);
   pinMode(IN2, OUTPUT);
 
+  pinMode(ledPin, OUTPUT);
+
   Serial.begin(9600);  // Baud rate set up
 }
 
 void loop() {
   if (!Run) {                     // If the variable 'Run' is False (0), execute the following code block
+    
+    // Pomp
+    digitalWrite(ledPin, HIGH);
+    
     analogWrite(ENA, 110);        // Set the motor speed using PWM with a value of 110
     digitalWrite(IN1, HIGH);      // Set the motor direction to forward
     digitalWrite(IN2, LOW);       // Ensure the motor direction is correct
@@ -24,12 +32,22 @@ void loop() {
     analogWrite(ENA, 0);          // Stop the motor by setting the speed to 0
     digitalWrite(IN1, HIGH);      // Prepare to change the motor direction
     digitalWrite(IN2, HIGH);      // Prepare to change the motor direction
+
+    digitalWrite(ledPin, LOW);
+
     delay(5000);                  // Wait for 5 seconds
+
+    // Valve 
+    digitalWrite(ledPin, HIGH);
 
     digitalWrite(relayPin, HIGH); // Open the relay
     delay(3000);                  // Wait for 3 seconds
 
     digitalWrite(relayPin, LOW);  // Close the relay
+
+    digitalWrite(ledPin, LOW);
+
+    // delay
     delay(5000);                  // Wait for 5 seconds
 
     Run = 1;                      // Set the variable 'Run' to True (1)
