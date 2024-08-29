54d4c9d31073322f17203512360763ef8bb1291b
wakky-alcedo <wakky.alcedo@gmail.com>
2024-08-29 14:08:39 +0800

    add: control method

--- contorl_hand_by_pc/control_hand_by_pc.py
+++ contorl_hand_by_pc/control_hand_by_pc.py
@@ -3,7 +3,19 @@ from nidaqmx.constants import AcquisitionType
 import serial
 import time
 
-port = "com9"  
+target_value:float = 2.1 # (constant)
+time_coefficient:float = 150.0 # (t.c.) (constant)
+proportional_gain:float = 150.0 # (p.g.) (constant)
+sensore_value:float = 0.0
+
+pomp_speed:int = 0
+is_valve_open:bool = False
+start_time = time.perf_counter()
+time_valve_open = 0.0
+
+is_feedback:bool = False
+
+port = "com4"  
 ser = serial.Serial(port, 9600, timeout=1)
 time.sleep(1)
 # Create a task
@@ -23,18 +35,40 @@ with nidaqmx.Task() as task:
         robot_status = 1
         while True:
             if robot_status == 1:
+                
+                # 計測 get voltage
                 data = task.read(number_of_samples_per_channel=1)
                 # Extract a single value from the list and format it
                 voltage = data[0]
                 print(f"Voltage: {voltage:.2f} V")
-                # Stop the task and send a signal to Arduino if voltage exceeds 2.1
-                if voltage > 2.1:
-                    print("Voltage exceeded 2.1, stopping task, sending signal to Arduino.")
-                    ser.write("ON".encode('utf-8'))
-                    task.stop()
-                    robot_status = 0
+                diff = target_value - voltage
+                
+                # 演算 control
+                if(diff > 0):
+                    pomp_speed = int(abs(diff) * proportional_gain)
+                    if pomp_speed > 255:
+                        pomp_speed = 255
+                    elif pomp_speed < 127:
+                        pomp_speed = 0
+                    is_valve_open = False
                 else:
-                    pass
+                    pomp_speed = 0
+                    if is_valve_open == True:
+                        if(start_time + time_valve_open < time.perf_counter()):
+                            is_valve_open = False
+                    else:
+                        # わんち閉じてから何秒間は開けないとかしたほうがいいかも
+                        time_valve_open = abs(diff) * time_coefficient
+                        start_time = time.perf_counter()
+                        is_valve_open = True
+
+                # 送信 send signal to Arduino
+                send_data = pomp_speed | int(is_valve_open)<<8
+                print(bin(send_data))
+                ser.write(chr(send_data).encode('utf-8'))
+
+                # 受信 get feedback from Arduino
+                print(ser.readline().decode('utf-8'))
             else:
                 robot_status = int(input("Type Robot Status (0/1): "))
     except KeyboardInterrupt:
